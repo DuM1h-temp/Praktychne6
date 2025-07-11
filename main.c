6939d1cf51a4d1545f8a248a377eac106cce371b
DuM1h <dumih00@gmail.com>
2025-07-11 15:14:31 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int x1, y1, x2, y2;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+
+    double length = sqrt(dx * dx + dy * dy);
+
+    printf("%.6f\n", length);
+
+    return 0;
+}
